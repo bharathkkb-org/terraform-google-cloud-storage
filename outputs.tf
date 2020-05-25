30fece41b1574b1249fee9d58f9f7d2f87bde045
Bharath KKB <bharathkrishnakb@gmail.com>
2020-05-25 14:55:00 -0500

    test from fork

--- outputs.tf
+++ outputs.tf
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-output "bucket" {
+output "bucketss" {
   description = "Bucket resource (for single use)."
   value       = google_storage_bucket.buckets[0]
 }
